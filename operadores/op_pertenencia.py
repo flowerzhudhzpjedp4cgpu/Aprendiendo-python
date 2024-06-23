c02417ec89af9a8f9fd96b33dc355edf38bc5889
Cristian Andres <flowerzagain@gmail.com>
2024-06-22 20:36:36 -0500

    Operadores de pertenencia

---
+++ operadores/op_pertenencia.py
@@ -0,0 +1,18 @@
+#identificar pertenencia en algunas secuencia
+a = [1,2,3,4,5]
+
+#esta 3 en la lista a?
+print(3 in a)#muestra True
+
+#no está 12 en la lista a?
+print(12 not in a)#muestra True
+
+str = "Hello World"
+
+#contiene World el string str?
+print("World" in str)#muestra True
+
+#contiene World el string str? (nota: distingue mayúsculas y minúsculas)
+print("world" in str)#muestra False
+
+print("code" not in str)#muestra True

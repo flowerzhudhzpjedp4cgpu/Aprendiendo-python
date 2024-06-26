a90f862284f01d0c588ab4e85757b88633dcf30e
Cristian Andres <flowerzagain@gmail.com>
2024-06-25 20:46:07 -0500

    Caso real de usar tipo diccionario

---
+++ dict_mutable/dict-caso-real.py
@@ -0,0 +1,48 @@
+secuencia = (
+    "nombres",
+    "apellidos",
+    "cedula",
+    "fecha_nacimiento",
+    "lugar_nacimiento",
+    "nacionalidad",
+    "estado_civil"          
+)
+
+versiones = dict.fromkeys(secuencia)
+
+print("\n\n Inscripción de Curso:")
+print("====================")
+print("\n Datos de participante")
+print("---------------------------")
+print("Keys : %s" % str(versiones))
+print("\n")
+
+try:#bloque de código a comprobar
+    cedula = int(input('Teclear cedula de identidad:'))#entrada de entero
+    
+    nombre = input('Teclear nombre:')#entrada  de cadena
+    apellido = input('Teclear apellido:')#entrada de cadena
+    fecha_nacimiento = input('Teclear fecha de nacimiento:')#entrada de cadena
+
+    versiones["cedula"] = cedula#asignar valor a clave - mutable
+    versiones["nombres"] = nombre
+    versiones["apellidos"] = apellido
+    versiones["fecha_nacimiento"] = fecha_nacimiento
+
+    print("\n")
+
+    print("Cédula de identidad: ", versiones["cedula"])
+    print("Nombre completo: ", versiones["nombres"] + " con apellido " + versiones["apellidos"])
+
+    import datetime, locale, os
+
+    print(
+    "Fecha de nacimiento:",
+    datetime.datetime.strftime(
+        datetime.datetime.strptime(versiones["fecha_nacimiento"], "%d/%m/%Y"),
+        "%d de %B de %Y",
+    )
+    )
+except:#bloque para captura de error
+    print('error, debe ser números')
+    
